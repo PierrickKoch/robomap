66ab15d6e166adec35ca6d640854a4322d6b9027
Pierrick Koch <pierrick.koch@gmail.com>
2013-10-10 16:43:10 +0200

    Add pom draw

---
+++ pom.py
@@ -0,0 +1,16 @@
+import json
+
+with open('pom.log') as f:
+    lines = f.readlines()
+
+path = []
+lines.pop(0) # remove header
+# date yaw pitch roll x y z
+for line in lines:
+  ll = line.split()
+  x = float(ll[4])
+  y = float(ll[5])
+  path.append([x,y])
+
+with open('pom.json', 'w') as f:
+    json.dump(path, f)
